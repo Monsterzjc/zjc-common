183c14341a624e414fbd31d89cbc04c92c1cc24c
zjc <1986263723@qq.com>
2022-10-29 14:08:23 +0800

    first commit

---
+++ src/main/java/com/zhang/demo/ZjcCommonApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjcCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjcCommonApplication.class, args);
+    }
+
+}
